69f14bac931c0273382e7dd6d07858c14f10e716
Nguyen Viet Hung <hungnguyen@remitano.com>
2022-10-31 21:31:46 +0700

    add notion clien wrapper

---
+++ lib/notion/database_client.rb
@@ -0,0 +1,42 @@
+class Notion::DatabaseClient
+  DEFAULT_SLEEP_TIME = 5
+  DEFAULT_MAX_RETRIES = 20
+
+  attr_accessor :database_id
+
+  def initialize(database_id)
+    self.database_id = database_id
+  end
+
+  def retrieve_pages
+    query_options = {
+      sleep_interval: DEFAULT_SLEEP_TIME,
+      max_retries: DEFAULT_MAX_RETRIES,
+      database_id: database_id
+    }
+    results = []
+    client.database_query(query_options) do |page_list|
+      pages = page_list.results
+      pages_without_formula_prop = pages.map do |page|
+        page["properties"] = page["properties"].reject { |_, prop| prop["type"] == "formula" }
+        page
+      end
+      results.concat(pages_without_formula_prop)
+    end
+    results
+  end
+
+  def update_page(page_id, object)
+    client.update_page(page_id, properties: object).present?
+  end
+
+  def create_page(object)
+    client.create_page(parent: {database_id: database_id}, properties: object).present?
+  end
+
+  private
+
+  def client
+    @client ||= Notion::Client.new
+  end
+end
